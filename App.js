a02174e1a18f03a4294fbac859bc434e62fe7e72
Jose Hernandez-Yepez <84255480+jose-hy@users.noreply.github.com>
2022-10-25 13:34:46 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Vocat</Text>
+      <Text>Study!</Text>
+      <Card>Log In</Card>
+      <Text>Or sign up here.</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
