fce6616d6ab36588439c311705ed65ded49651a3
Yoshiaki Kato <katoy01@nyu.edu>
2022-10-31 22:32:16 -0400

    Created Home and Settings page with font size interactability

--- VocatProject/App.js
+++ VocatProject/App.js
@@ -6,112 +6,41 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import type { Node } from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { TouchableOpacity, DeviceEventEmitter, StyleSheet, Button, SafeAreaView, Text, Alert, useColorScheme, View, } from 'react-native';
+import Slider from '@react-native-community/slider';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+const Stack = createNativeStackNavigator();
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({ children, title }): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+/**
+ * Components
+ */
+import HomeScreen from './src/HomeScreen.js';
+import SettingsScreen from './src/SettingsScreen.js';
 
+
+/**
+ * App.js will be used for navigating between pages
+ */
 const App: () => Node = () => {
   const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  // setSettings({
+  //   backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  //   textSize: 10
+  // });
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Settings" component={SettingsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
---
+++ VocatProject/src/SettingsScreen.js
@@ -0,0 +1,114 @@
+import React, { useEffect, useState } from 'react';
+import type { Node } from 'react';
+import Slider from '@react-native-community/slider';
+import { TouchableOpacity, DeviceEventEmitter, StyleSheet, Button, Text, Alert, useColorScheme, View, } from 'react-native';
+
+
+const SettingsScreen = ({ props, navigation, route }) => {
+    const [finalSize, setFinalSize] = useState(route.params.settings.textSize);
+
+    function style(options) {
+        return {
+            fontSize: options.textSize
+        }
+    }
+
+    return (
+        <View style={styles.settingsContainer}>
+            <Text style={style(route.params.settings)}>Text fontSize: {finalSize}</Text>
+            <Slider
+                id='fontSizeSlider'
+                minimumValue={10}
+                maximumValue={50}
+                step={1}
+                value={finalSize}
+                onValueChange={value => setFinalSize(value)}
+                style={styles.slider}
+            />
+            <TouchableOpacity style={[styles.button]}
+                onPress={() => {
+                    let temp = route.params.settings;
+                    temp.textSize = finalSize;
+                    DeviceEventEmitter.emit("event.changeSettings", temp);
+                }}>
+                <Text style={{ fontSize: 20 }}>Done</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={[styles.bigButton]}
+                onPress={() => {
+                    navigation.navigate('Home', { settings: route.params.settings });
+                }}>
+                <Text style={[styles.buttonText]}>Help</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={[styles.bigButton]}
+                onPress={() => {
+                    navigation.navigate('Home', { settings: route.params.settings });
+                }}>
+                <Text style={[styles.buttonText]}>Feedback</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={[styles.button, {
+                position: 'absolute',
+                bottom: 20
+            }]}
+                onPress={() => {
+                    navigation.navigate('Home', { settings: route.params.settings });
+                }}>
+                <Text style={{ fontSize: 30 }}>Home</Text>
+            </TouchableOpacity>
+        </View >
+    );
+};
+
+
+const styles = StyleSheet.create({
+    settingsContainer: {
+        backgroundColor: '#FEFAE0',
+        width: '100%',
+        height: '100%',
+        flex: 1,
+        alignItems: 'center'
+    },
+    // sliderContainer: {
+    //     width: '100%',
+    //     flexDirection: 'row'
+    // },
+    slider: {
+        backgroundColor: '#D3D3D3',
+        width: '100%',
+        marginTop: 20,
+        marginBottom: 10,
+        padding: 5,
+    },
+    bigButton: {
+        backgroundColor: '#CCD5AE',
+        borderRadius: 30,
+        padding: 5,
+        alignItems: 'center',
+        width: 250,
+        marginBottom: 10,
+    },
+    buttonText: {
+        fontSize: 30
+    },
+    button: {
+        backgroundColor: '#CCD5AE',
+        borderRadius: 10,
+        padding: 5,
+        alignItems: 'center',
+        marginBottom: 10,
+    },
+    headerContainer: {
+        flexDirection: 'row'
+    },
+    message: {
+        marginTop: '30%',
+        marginBottom: 20,
+    },
+    content: {
+        flex: 1,
+        flexDirection: 'column',
+        alignContent: 'center',
+        alignItems: 'center'
+    }
+});
+
+export default SettingsScreen;
