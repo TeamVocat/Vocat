231fbd23286d8899ba13703c8809894f1362d37f
G <gg2484@nyu.edu>
2022-11-08 13:30:05 -0500

    added store page: foods

---
+++ VocatProject/client/assets/Images.js
@@ -0,0 +1,14 @@
+export default {
+  general: {
+    catpile: require('./cat_pile.png'),
+    catcake: require('./cat_cakes.png'),
+  },
+  foods: {
+    food1: require('./foods/food1.png'),
+    food2: require('./foods/food2.png'),
+    food3: require('./foods/food3.png'),
+    food4: require('./foods/food4.png'),
+    food5: require('./foods/food5.png'),
+    food6: require('./foods/food6.png'),
+  },
+};
--- VocatProject/client/src/StoreScreen.js
+++ VocatProject/client/src/StoreScreen.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import Slider from "@react-native-community/slider";
+import React, {useEffect, useState} from 'react';
+import Slider from '@react-native-community/slider';
 import {
   Image,
   TouchableOpacity,
@@ -11,64 +11,88 @@ import {
   useColorScheme,
   View,
   ScrollView,
-} from "react-native";
-import { Row, Rows, Table, TableWrapper } from "react-native-table-component";
-import catPile from "./../assets/cat_pile.png";
-import { Shadow } from "react-native-shadow-2";
+} from 'react-native';
+import {Row, Rows, Table, TableWrapper} from 'react-native-table-component';
+import {Images} from '../assets/';
+import {Shadow} from 'react-native-shadow-2';
 
-const StoreScreen = ({ navigation, route }) => {
-  const [settings, setSettings] = useState({ textSize: 30 });
-  const state = {
-    tableHead: ["Food", "Toys"],
-    tableData: [
-      ["1", "2", "3"],
-      ["a", "b", "c"],
-      ["1", "2", "3"],
-      ["a", "b", "c"],
-    ],
-  };
+const StoreScreen = ({navigation, route}) => {
+  const [settings, setSettings] = useState({textSize: 30});
+  const foods = ['food1', 'food2', 'food3', 'food4', 'food5', 'food6'];
 
   return (
     <View style={styles.storeContainer}>
       <View id="header" style={styles.header}>
         <Shadow
           distance={5}
-          startColor={"#E6E5DAED"}
-          endColor={"#FEFAE090"}
+          startColor={'#E6E5DAED'}
+          endColor={'#FEFAE090'}
           paintInside={true}
-          containerViewStyle={{ margin: 100 }}
+          containerViewStyle={{margin: 100}}
           safeRender={true}
           style={{
             borderTopStartRadius: 5,
             borderRadius: 2,
           }}
-          offset={[2, 2]}
-        >
+          offset={[-2, 7]}>
           <TouchableOpacity
-            style={[styles.button]}
+            style={[styles.button, {left: -5, top: 5}]}
             onPress={() => {
-              navigation.navigate("Settings", { settings: settings });
-            }}
-          >
-            <Text style={{ fontSize: 30 }}>Store</Text>
+              navigation.navigate('CatHouse', {settings: settings});
+            }}>
+            <Text style={{fontSize: 30}}>Back</Text>
           </TouchableOpacity>
         </Shadow>
       </View>
-      <View id="table" style={styles.tableContainer}>
-        <ScrollView style={{ height: 180 }}>
-          <Table borderStyle={{ borderWidth: 5, borderColor: "#D3D3D3" }}>
-            {state.tableData.map((rowData, index) => (
-              <Row
-                key={index}
-                data={rowData}
-                style={[
-                  styles.tableRows,
-                  index % 2 && { backgroundColor: "#EBEDDF" },
-                ]}
-                textStyle={styles.tableText}
-              />
-            ))}
-          </Table>
+      <View id="contents" style={styles.contents}>
+        <ScrollView style={{height: 100}}>
+          <View id="Foodtitle">
+            <Text style={styles.titleText}>FOODS</Text>
+          </View>
+          <View id="Foods" style={styles.imgContainer}>
+            <View key={1} style={styles.imgItemWrap}>
+              <Image
+                source={Images.foods.food1}
+                style={{
+                  height: 110,
+                  width: 110,
+                }}
+                resizeMode="contain"></Image>
+              <Text style={styles.text}>Soup</Text>
+            </View>
+            <View key={2} style={styles.imgItemWrap}>
+              <Image
+                source={Images.foods.food2}
+                style={{
+                  height: 110,
+                  width: 110,
+                }}
+                resizeMode="contain"></Image>
+              <Text style={styles.text}>Soup</Text>
+            </View>
+            <View key={3} style={styles.imgItemWrap}>
+              <Image
+                source={Images.foods.food3}
+                style={{
+                  height: 110,
+                  width: 110,
+                }}
+                resizeMode="contain"></Image>
+              <Text style={styles.text}>Soup</Text>
+            </View>
+            {foods.map((x, i) => {
+              <View key={i} style={styles.imgItemWrap}>
+                <Image
+                  source={Images.foods.x}
+                  style={{
+                    height: 130,
+                    width: 130,
+                  }}
+                  resizeMode="contain"></Image>
+                <Text style={styles.text}>{x}</Text>
+              </View>;
+            })}
+          </View>
         </ScrollView>
       </View>
     </View>
@@ -77,39 +101,40 @@ const StoreScreen = ({ navigation, route }) => {
 
 const styles = StyleSheet.create({
   storeContainer: {
-    backgroundColor: "#FEFAE0",
-    width: "100%",
-    height: "100%",
+    backgroundColor: '#FEFAE0',
+    width: '100%',
+    height: '100%',
     flex: 1,
-    flexDirection: "column",
-    alignContent: "center",
-    alignItems: "center",
+    flexDirection: 'column',
+    alignContent: 'center',
+    alignItems: 'center',
   },
   header: {
-    width: "90%",
+    width: '90%',
     marginTop: 10,
-    backgroundColor: "#FEFAE0",
-    flexDirection: "row",
+    backgroundColor: '#FEFAE0',
+    flexDirection: 'row',
     flex: 1,
-    justifyContent: "flex-end",
-    alignItems: "flex-end",
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
   },
-  tableContainer: {
-    width: "100%",
+  contents: {
+    width: '100%',
     paddingTop: 20,
-    backgroundColor: "#DCDCDC",
-    flexDirection: "column",
-    alignContent: "center",
-    alignItems: "center",
-    flex: 8,
+    backgroundColor: '#DCDCDC',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    alignContent: 'center',
+    flex: 6,
   },
   button: {
-    paddingHorizontal: 25,
-    paddingVertical: 10,
+    paddingHorizontal: 15,
+    paddingVertical: 5,
     borderRadius: 15,
-    alignSelf: "flex-start",
-    textAlign: "center",
-    backgroundColor: "#CCD5AE",
+    alignSelf: 'flex-start',
+    textAlign: 'center',
+    backgroundColor: '#CCD5AE',
   },
   headerButtonText: {
     fontSize: 20,
@@ -117,18 +142,27 @@ const styles = StyleSheet.create({
   buttonText: {
     fontSize: 30,
   },
-
-  tableHead: {
-    height: 40,
-    backgroundColor: "#DCDCDC",
+  imgContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignContent: 'center',
+    alignItems: 'center',
+    flexWrap: 'wrap',
   },
-  tableRows: {
-    backgroundColor: "#DFE0D5",
+  imgItemWrap: {
+    backgroundColor: '#FFFFFF',
+    flexDirection: 'column',
+    margin: 10,
   },
-  tableText: {
-    height: 50,
+  text: {
     fontSize: 20,
-    textAlign: "center",
+    textAlign: 'center',
+  },
+  titleText: {
+    fontSize: 30,
+    fontStyle: 'bold',
+    textAlign: 'center',
   },
 });
 
