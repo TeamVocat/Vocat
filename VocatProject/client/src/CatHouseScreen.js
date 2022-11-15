012a850b6e054867d90e5e4a445f7e0e7478cf55
G <gg2484@nyu.edu>
2022-11-15 14:29:53 -0500

    added actions

--- VocatProject/client/src/CatHouseScreen.js
+++ VocatProject/client/src/CatHouseScreen.js
@@ -10,6 +10,7 @@ import {
   useColorScheme,
   View,
   ScrollView,
+  Pressable,
 } from 'react-native';
 import {Row, Rows, Table, TableWrapper} from 'react-native-table-component';
 import {Images} from '../assets/';
@@ -25,6 +26,75 @@ const CatHouseScreen = ({navigation, route}) => {
       ['1', '2', '3'],
       ['a', 'b', 'c'],
     ],
+  };
+  const [catState, setCatState] = useState('lying');
+  const sleepClick = () => {
+    setCatState('sleeping');
+  };
+  const waterClick = () => {
+    setCatState('drinking');
+  };
+  const catClick = () => {
+    setCatState('lying');
+  };
+  const showCat = () => {
+    return (
+      <Pressable onPress={catClick}>
+        <Image
+          source={Images.actions[catState]}
+          style={{
+            width: 300,
+          }}
+          resizeMode="contain"></Image>
+      </Pressable>
+    );
+  };
+
+  const [ItemState, setItemState] = useState('foods');
+  const foods = ['food1', 'food2', 'food3', 'food4', 'food5', 'food6'];
+  const toys = ['food4', 'food5', 'food6', 'food1', 'food2', 'food3'];
+
+  const foodClick = () => {
+    setItemState('foods');
+  };
+  const toyClick = () => {
+    setItemState('toys');
+  };
+
+  const showTable = () => {
+    if (ItemState === 'foods') {
+      return (
+        <View id="Foods" style={styles.imgContainer}>
+          {foods.map((x, i) => (
+            <View key={i} style={styles.imgItemWrap}>
+              <Image
+                source={Images.foods[x]}
+                style={{
+                  height: 110,
+                  width: 110,
+                }}
+                resizeMode="contain"></Image>
+            </View>
+          ))}
+        </View>
+      );
+    } else if (ItemState === 'toys') {
+      return (
+        <View id="Toys" style={styles.imgContainer}>
+          {toys.map((x, i) => (
+            <View key={i} style={styles.imgItemWrap}>
+              <Image
+                source={Images.foods[x]}
+                style={{
+                  height: 110,
+                  width: 110,
+                }}
+                resizeMode="contain"></Image>
+            </View>
+          ))}
+        </View>
+      );
+    }
   };
 
   return (
@@ -54,37 +124,24 @@ const CatHouseScreen = ({navigation, route}) => {
         <View
           id="cats"
           style={{flex: 4, alignItems: 'center', justifyContent: 'center'}}>
-          <Image
-            source={Images.general.catpile}
-            style={{
-              width: 300,
-            }}
-            resizeMode="contain"></Image>
+          {showCat()}
         </View>
-        <View style={styles.tableContainer}>
-          <Table borderStyle={{borderWidth: 0, borderColor: '#C1C0B9'}}>
-            <Row
-              data={state.tableHead}
-              style={styles.tableHead}
-              textStyle={styles.tableText}
-              flexArr={[1, 2]}
-            />
-            <ScrollView style={{height: 180}}>
-              <Table borderStyle={{borderWidth: 5, borderColor: '#D3D3D3'}}>
-                {state.tableData.map((rowData, index) => (
-                  <Row
-                    key={index}
-                    data={rowData}
-                    style={[
-                      styles.tableRows,
-                      index % 2 && {backgroundColor: '#EBEDDF'},
-                    ]}
-                    textStyle={styles.tableText}
-                  />
-                ))}
-              </Table>
-            </ScrollView>
-          </Table>
+        <View id="controls" style={{flex: 0.8, flexDirection: 'row'}}>
+          <TouchableOpacity style={[styles.controlBotton]} onPress={sleepClick}>
+            <Text style={{fontSize: 20}}>Sleep</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.controlBotton]} onPress={waterClick}>
+            <Text style={{fontSize: 20}}>Water</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.controlBotton]} onPress={foodClick}>
+            <Text style={{fontSize: 20}}>Foods</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.controlBotton]} onPress={toyClick}>
+            <Text style={{fontSize: 20}}>Toys</Text>
+          </TouchableOpacity>
+        </View>
+        <View id="table" style={[styles.tableContainer]}>
+          <ScrollView style={{height: 100}}>{showTable()}</ScrollView>
         </View>
       </View>
       <View id="footer" style={[styles.footer]}>
@@ -186,6 +243,30 @@ const styles = StyleSheet.create({
     height: 50,
     fontSize: 20,
     textAlign: 'center',
+  },
+  imgContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignContent: 'center',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+  },
+  imgItemWrap: {
+    backgroundColor: '#FFFFFF',
+    flexDirection: 'column',
+    margin: 10,
+  },
+  controlBotton: {
+    paddingHorizontal: 15,
+    paddingVertical: 5,
+    borderRadius: 0,
+    margin: 5,
+    alignSelf: 'flex-start',
+    textAlign: 'center',
+    backgroundColor: '#CCD5AE',
+    alignContent: 'center',
+    alignItems: 'center',
   },
 });
 
