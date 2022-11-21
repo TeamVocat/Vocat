dfff9ac48a7517f0bfa1d29ac414feef9a0a8da8
SiyuBi <109042516+SiyuBi@users.noreply.github.com>
2022-11-21 09:28:38 -0500

    Delete VocatProject/client/src/functions directory

--- VocatProject/client/src/functions/Words.js
+++
@@ -1,8 +0,0 @@
-import {review, learn, grab, UserWord, UserWordBank, store, retrieve} from './Functions.js';
-
-let screens = {};
-async(){
-  await learn(screens);
-  console.log(screens);
-}
-export default {screens};
