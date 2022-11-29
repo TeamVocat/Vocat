4b7255f885c0497f98e7281b58e50caaabd3c394
SiyuBi <109042516+SiyuBi@users.noreply.github.com>
2022-11-29 13:32:07 -0500

    Connected Progress Screen

--- VocatProject/client/src/ProgressScreen.js
+++ VocatProject/client/src/ProgressScreen.js
@@ -3,13 +3,27 @@ import { Dimensions, TouchableOpacity, DeviceEventEmitter, StyleSheet, Button, T
 import { Picker } from '@react-native-picker/picker';
 import { LineChart } from "react-native-chart-kit";
 import Svg, { Circle, Ellipse, G, LinearGradient, RadialGradient, Line, Path, Polygon, Polyline, Rect, Symbol, Use, Defs, Stop } from 'react-native-svg';
+import { retrieveProgress } from './Functions.js';
 
 const date = "11/1";
-const wordsLearned = 130;
 const wordsNotLearned = 870;
 
 const ProgressScreen = ({ props, navigation, route }) => {
-    const [finalSize, setFinalSize] = useState(route.params.settings.textSize);
+    const [labels, setLabels] = useState(['11.1','11.2','11.3','11.4','11.5']);
+    const [progresses, setProgresses] = useState([1,2,3,4,5]);
+
+    useEffect(() => {
+        async function fetchMessage() {
+            try {
+              let progressArray = await retrieveProgress();
+              progressArray = progressArray.slice(progressArray.length-6);
+              setProgresses(progressArray);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        fetchMessage();
+    }, []);
 
     let data = [{ value: 'book1' }];
     //let newWords = 20;
@@ -17,18 +31,18 @@ const ProgressScreen = ({ props, navigation, route }) => {
     const [newWords, numNewWords] = React.useState('20');
     const [oldWords, numOldWords] = React.useState('30');
     const chartConfig = {
-        backgroundGradientFrom: '#FEFAE0',
-        backgroundGradientTo: '#FEFAE0',
+        backgroundGradientFrom: '#ffffff',
+        backgroundGradientTo: '#ffffff',
         color: (opacity = 1) => 'black',
         strokeWidth: 2, // optional, default 3
         barPercentage: 0.5
     };
     const screenWidth = Dimensions.get("window").width;
     const chartData = {
-        labels: ["11/1", "11/2", "11/3", "11/4", "11/5", "11/6"],
+        labels: labels,
         datasets: [
             {
-                data: [20, 45, 55, 80, 99, 130],
+                data: progresses,
                 color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, // optional
                 strokeWidth: 2 // optional
             }
@@ -36,7 +50,6 @@ const ProgressScreen = ({ props, navigation, route }) => {
         legend: ["Words Learned"] // optional
     };
 
-    const [selectedValue, setSelectedValue] = useState("java");
     return (
         <View style={styles.settingsContainer}>
             <TouchableOpacity style={styles.button}
@@ -54,20 +67,10 @@ const ProgressScreen = ({ props, navigation, route }) => {
             />
 
             <Text style={styles.buttonLabel}>Number of words learned:</Text>
-            <Text style={styles.buttonLabel}>{wordsLearned}</Text>
+            <Text style={styles.buttonLabel}>{progresses[progresses.length-1]}</Text>
 
             <Text style={styles.buttonLabel}>Number of words to be learned:</Text>
             <Text style={styles.buttonLabel}>{wordsNotLearned}</Text>
-
-            <TouchableOpacity style={[styles.button, {
-                position: 'absolute',
-                bottom: 20
-            }]}
-                onPress={() => {
-                    navigation.navigate('Home', { settings: route.params.settings });
-                }}>
-                <Text style={{ fontSize: 30 }}>Home</Text>
-            </TouchableOpacity>
         </View >
     );
 };
@@ -75,31 +78,10 @@ const ProgressScreen = ({ props, navigation, route }) => {
 
 const styles = StyleSheet.create({
     settingsContainer: {
-        backgroundColor: '#FEFAE0',
         width: '100%',
         height: '100%',
         flex: 1,
         alignItems: 'center'
-    },
-    // sliderContainer: {
-    //     width: '100%',
-    //     flexDirection: 'row'
-    // },
-    picker: {
-        backgroundColor: '#FFFFFF',
-        width: '50%',
-        marginTop: 10,
-        marginBottom: 10,
-        padding: 5,
-        flexDirection: 'row',
-        flexWrap: 'wrap'
-    },
-    input: {
-        backgroundColor: 'white',
-        width: 60,
-        margin: 12,
-        borderWidth: 1,
-        paddingTop: 10
     },
     buttonText: {
         fontSize: 30
