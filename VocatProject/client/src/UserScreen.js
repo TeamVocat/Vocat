e7d066e6413c03c0bfa727aaf09f9f6e3a5214fb
G <gg2484@nyu.edu>
2022-12-05 21:37:01 -0500

    added settings page

--- VocatProject/client/src/UserScreen.js
+++ VocatProject/client/src/UserScreen.js
@@ -28,6 +28,8 @@ import {Images} from '../assets/';
 import {LineChart} from 'react-native-chart-kit';
 import SettingsScreen from './SettingsScreen';
 import SelectDropdown from 'react-native-select-dropdown';
+import Slider from '@react-native-community/slider';
+import {FontSize, Language} from 'iconoir-react-native';
 
 const UserScreen = ({navigation, route}) => {
   const layout = useWindowDimensions();
@@ -179,11 +181,246 @@ const UserScreen = ({navigation, route}) => {
     </View>
   );
 
-  const SettingsRoute = () => (
-    <View style={{flex: 1, backgroundColor: '#FFFFFF'}}>
-      {SettingsScreen(navigation)}
-    </View>
-  );
+  const SettingsRoute = () => {
+    const [finalSize, setFinalSize] = useState();
+    const [settings, setSettings] = useState({});
+    const [user, setUser] = useState({});
+
+    useEffect(() => {
+      fetchSettingsUser();
+    }, []);
+
+    const handleSync = async () => {
+      setSettings({
+        ...settings,
+        textSize: finalSize,
+      });
+      try {
+        await storeSettings(settings);
+      } catch (err) {
+        console.log('Settings could not be stored locally', err);
+      }
+      try {
+        console.log(user);
+        await axios.post(`${REACT_APP_SERVER_HOSTNAME}/api/updateUser`, {user});
+        alert('Successfully Synced User to Database');
+      } catch (err) {
+        console.log('Could not update user', err);
+      }
+    };
+
+    const fetchSettings = async () => {
+      console.log(`Fetching Settings from local storage...`);
+      try {
+        let temp_settings = await getSettings();
+        if (temp_settings) {
+          // console.log("new settings:", temp_settings);
+          setSettings(temp_settings);
+          setFinalSize(temp_settings.textSize);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    const fetchSettingsUser = async () => {
+      console.log(`Fetching Settings and User from local storage...`);
+      try {
+        let temp_settings = await getSettings();
+        if (temp_settings) {
+          // console.log("new settings:", temp_settings);
+          setSettings(temp_settings);
+          setFinalSize(temp_settings.textSize);
+        }
+        let temp_user = await getUserLocal();
+        if (temp_user) {
+          // console.log("new user:", temp_user);
+          setUser(temp_user);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    function style(options) {
+      return {
+        fontSize: options.textSize,
+      };
+    }
+
+    const languages = ['English', 'German', 'Spanish'];
+
+    return (
+      <View
+        style={{
+          flex: 1,
+          height: '100%',
+          width: '100%',
+          justifyContent: 'center',
+          alignContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'white',
+        }}>
+        <ScrollView style={{width: '100%'}}>
+          <View
+            style={{
+              flex: 1,
+              justifyContent: 'center',
+              alignContent: 'center',
+              alignItems: 'center',
+            }}>
+            <View
+              class={'font size'}
+              style={{
+                flex: 2,
+                height: '100%',
+                width: '100%',
+                justifyContent: 'center',
+                alignContent: 'center',
+                alignItems: 'center',
+                backgroundColor: 'white',
+              }}>
+              <View
+                style={{
+                  width: '100%',
+                  height: 30,
+                  left: 15,
+                  top: 10,
+                }}>
+                <Text style={{color: '#AAAAAA', fontSize: 14}}>
+                  Personalize
+                </Text>
+              </View>
+              <View
+                style={{
+                  flex: 1,
+                  height: 50,
+                  width: '100%',
+                  backgroundColor: '#D9D9D9',
+                  alignContent: 'center',
+                  alignItems: 'center',
+                  flexDirection: 'row',
+                }}>
+                <FontSize
+                  color="black"
+                  height={25}
+                  width={25}
+                  style={{marginHorizontal: 10, top: 2, left: 10}}
+                />
+                <Text style={{fontSize: 20, color: 'black', left: 10}}>
+                  Font Size
+                </Text>
+                <TouchableOpacity
+                  style={[styles.button, {left: 180}]}
+                  onPress={handleSync}>
+                  <Text style={{fontSize: 20}}>Done</Text>
+                </TouchableOpacity>
+              </View>
+              <Slider
+                id="fontSizeSlider"
+                minimumValue={15}
+                maximumValue={50}
+                step={1}
+                value={finalSize}
+                onValueChange={value => setFinalSize(value)}
+                style={{
+                  backgroundColor: '#EFEFEF',
+                  height: 50,
+                  width: '100%',
+                  padding: 10,
+                }}
+              />
+              <View
+                style={{
+                  width: '100%',
+                  backgroundColor: '#EFEFEF',
+                  justifyContent: 'flex-start',
+                  alignContent: 'center',
+                  alignItems: 'center',
+                  paddingBottom: 20,
+                }}>
+                <Text style={style(settings)}>Text fontSize: {finalSize}</Text>
+              </View>
+            </View>
+            <View
+              class={'language menu'}
+              style={{flex: 1, width: '100%', marginBottom: 20}}>
+              <View
+                style={{
+                  width: '100%',
+                  height: 30,
+                  left: 15,
+                  top: 10,
+                }}>
+                <Text style={{color: '#AAAAAA', fontSize: 14}}>Learning</Text>
+              </View>
+              <View
+                style={{
+                  flex: 1,
+                  height: 50,
+                  width: '100%',
+                  backgroundColor: '#D9D9D9',
+                  alignContent: 'center',
+                  alignItems: 'center',
+                  flexDirection: 'row',
+                }}>
+                <Language
+                  color="black"
+                  height={25}
+                  width={25}
+                  style={{marginHorizontal: 10, top: 1, left: 10}}
+                />
+                <Text style={{fontSize: 20, color: 'black', top: -1, left: 10}}>
+                  Language
+                </Text>
+              </View>
+              <SelectDropdown
+                data={languages}
+                defaultValueByIndex={0}
+                onSelect={(selectedItem, index) => {
+                  console.log(selectedItem, index);
+                }}
+                buttonTextAfterSelection={(selectedItem, index) => {
+                  return selectedItem;
+                }}
+                rowTextForSelection={(item, index) => {
+                  return item;
+                }}
+                buttonStyle={{
+                  width: '100%',
+                  height: 50,
+                }}
+              />
+            </View>
+            <View
+              class={'buttons'}
+              style={{flex: 0.5, flexDirection: 'row', margin: 5}}>
+              <TouchableOpacity
+                style={[
+                  styles.bigButton,
+                  {backgroundColor: '#2A9D8F', marginHorizontal: 10},
+                ]}
+                onPress={() => {
+                  navigation.navigate('Home');
+                }}>
+                <Text style={{fontSize: 23, color: 'white'}}>Sync</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={[
+                  styles.bigButton,
+                  {backgroundColor: '#AAAAAA', marginHorizontal: 10},
+                ]}
+                onPress={() => {
+                  navigation.navigate('Home');
+                }}>
+                <Text style={{fontSize: 23}}>Sign Out</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </ScrollView>
+      </View>
+    );
+  };
 
   const renderScene = SceneMap({
     progress: ProgressRoute,
@@ -244,6 +481,43 @@ const UserScreen = ({navigation, route}) => {
 const styles = StyleSheet.create({
   text: {
     fontSize: 16,
+    color: 'black',
+  },
+  settingsContainer: {},
+  // sliderContainer: {
+  //     width: '100%',
+  //     flexDirection: 'row'
+  // },
+  bigButton: {
+    alignItems: 'center',
+    width: 160,
+    height: 50,
+    color: 'white',
+    padding: 5,
+    borderRadius: 15,
+    margin: 5,
+    justifyContent: 'center',
+  },
+  buttonText: {
+    fontSize: 30,
+  },
+  button: {
+    backgroundColor: '#EFEFEF',
+    margin: 5,
+    paddingHorizontal: 10,
+  },
+  headerContainer: {
+    flexDirection: 'row',
+  },
+  message: {
+    marginTop: '30%',
+    marginBottom: 20,
+  },
+  content: {
+    flex: 1,
+    flexDirection: 'column',
+    alignContent: 'center',
+    alignItems: 'center',
   },
 });
 
